a556c988f47a1bb2dbca653e999b1bb1d96e340e
Iulia Puiu <iulia.puiu@hotmail.com>
2024-01-02 13:46:55 +0200

    Install mmkv and expo prebuild

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text, Button, Image } from 'react-native'
 import React, { useState } from 'react'
 import * as ImagePicker from 'expo-image-picker';
+import { useMMKVString } from 'react-native-mmkv';
 
 const index = () => {
 
     const [image, setImage] = useState<string | null>(null)
+    const [username, setUsername] = useMMKVString('user.name')
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,9 +25,10 @@ const index = () => {
 
     return (
         <View>
-            <Text>index</Text>
             <Button title="Pick an image from camera roll" onPress={pickImage} />
             {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+            <Text>Username: {username}</Text>
+            <Button title="setUsername" onPress={() => setUsername('John Doe')} />
         </View>
     )
 }
